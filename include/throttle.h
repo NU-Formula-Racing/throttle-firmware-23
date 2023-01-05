9ee30451a2aa0029e6f4aae358dd7f8af60ec3df
Adam <83994695+adampchen@users.noreply.github.com>
2023-01-04 20:37:27 -0500

    Ac/set up code skeletons (#1)
    * Added include for virtualTimer and minor fixes
    
    * fixed libdep issues
    
    * Added additional documentation for Throttle functions and edited CAN signals and message
    
    * Added comments marking off sections that need review/changes
    
    * Updated CAN signal/message to reflect dbc file. Wrote pseudocode/untested code for virtual timer and most Throttle methods
    
    * Added testing code in ReadAcceleratorPress() for reading sensor and converting to throttle percentage. Some minor adjustments in main and throttle.h
    
    * Implemented function for converting throttle percentage into torque (x - 10) was changed to (x- 9). Got CAN bus reads and transmits to work properly.
    
    Co-authored-by: adamc2024 <83994695+adamc2024@users.noreply.github.com>
    Co-authored-by: izo0619 <isabelzhong30@gmail.com>

---
+++ include/throttle.h
@@ -0,0 +1,36 @@
+/**
+ * @brief Definitions and constants for the throttle.
+ * 
+ */
+class Throttle
+{
+    public:
+        // Need to include constants
+        /*
+        static constexpr int throttleSensorPin =;
+        */
+        Throttle();
+        // Reads how much the accelerometer is being pressed (a percentage
+        // from 0 to 100)
+        uint16_t ReadAcceleratorPress();
+
+        // Returns true if the two potentiometers on the acceleration pedal
+        // agree with each other
+        bool arePotentiometersCorrect();
+
+        // Returns true if the brake and accelerator pedals are simultaneously pressed,
+        // which would result in the throttle being set to 0 percent
+        bool brakeAndAccelerator();
+
+        void updateBrakePosition();
+
+    private:
+        //// Might not need all of these private fields
+
+        long brake_position;
+        // Both potentiometer voltages must be within a threshold
+        long left_accelerometer_position;
+        long right_accelerometer_position;
+        // Will be communicated to the CAN bus
+        uint16_t cur_throttle_signal;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,71 @@
 #include <Arduino.h>
+#include "virtualTimer.h"
+#include "throttle.h"
+
+#define SERIAL_DEBUG
+
+#if defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY41)
+#include "teensy_can.h"
+// The bus number is a template argument for Teensy: TeensyCAN<bus_num>
+TeensyCAN<1> can_bus{};
+#endif
+
+#ifdef ARDUINO_ARCH_ESP32
+#include "esp_can.h"
+// The tx and rx pins are constructor arguments to ESPCan, which default to TX = 5, RX = 4
+ESPCAN can_bus{};
+#endif
+
+// Initialize board
+Throttle throttle;
+
+// Structure for handling timers
+VirtualTimerGroup read_timer;
+
+// TX CAN Signal
+CANSignal<uint8_t, 0, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> cur_throttle_signal{};
+
+// TX CAN Message (Might need to edit transmit period (currently 100))
+CANTXMessage<1> tx_message{can_bus, 0x300, 1, 100, read_timer, cur_throttle_signal};
+
+CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(-40), false> motor_temp_signal{};
+CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(-40), false> coolant_temp_signal{};
+CANSignal<float, 32, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(-40), false> ambient_temp_signal{};
+CANRXMessage<3> rx_message{can_bus, 0x420, motor_temp_signal, coolant_temp_signal, ambient_temp_signal};
+
+void ReadAcceleratorPress() {
+	cur_throttle_signal = throttle.ReadAcceleratorPress();
+	Serial.print("cur_throttle_signal: ");
+	Serial.println(cur_throttle_signal);
+	Serial.println("\n");
+};
+
+void printReceiveSignals() {
+	can_bus.Tick();
+	Serial.print("motor_temp_signal");
+	Serial.println((float)motor_temp_signal);
+	Serial.print("coolant_temp_signal");
+	Serial.println((float)coolant_temp_signal);
+	Serial.print("ambient_temp_signal");
+	Serial.println((float)ambient_temp_signal);
+	Serial.println("\n");
+};
 
 void setup() {
-  // put your setup code here, to run once:
+	#ifdef SERIAL_DEBUG
+	// Initialize serial output 
+	Serial.begin(9600); // Baud rate (Can transfer max of 9600 bits/second)
+	#endif
+
+	// Initialize can bus
+	can_bus.Initialize(ICAN::BaudRate::kBaud1M);
+
+	//Initialize our timer(s)
+	read_timer.AddTimer(100, ReadAcceleratorPress);
+	read_timer.AddTimer(100, printReceiveSignals);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	delay(0);
+	read_timer.Tick(millis());
 }
